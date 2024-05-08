cc82c0d82aa5aacd5216f8aba636df4f8a6cdc42
Tonyrealzy <umehobiarinze2@gmail.com>
2024-05-08 17:21:53 +0100

    Attempting to deploy my SPA on Github Pages

---
+++ 0x01-react_intro/task_3/dashboard/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import Logo from './HolbertonLogo.jpg';
+import { getFooterCopy, getFullYear } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-header'>
+        <img src={Logo} className='App-logo' alt='Holberton Logo' />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <aside>
+          <label htmlFor='email'>
+            Email:
+          </label>
+          <input type='email' id='email' />
+
+          <label htmlFor='password'>
+            Password:
+          </label>
+          <input type='password' id='password' />
+
+          <button>OK</button>
+        </aside>
+      </div>
+
+      <div className='App-footer'>
+        <p>Copyright { getFullYear() } - { getFooterCopy() }</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
